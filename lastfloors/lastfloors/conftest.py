52e668a4fa30e3cc714ef22df290104a2dd26326
CoderMungan <codermungan@gmail.com>
2024-04-03 23:24:24 +0300

    docs(initial): initial files django

---
+++ lastfloors/lastfloors/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from lastfloors.users.models import User
+from lastfloors.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
